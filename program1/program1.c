fcf3e13869e31f3b4914b5ba7979eb4e491d7454
saccharin98 <xinyanzhou938@gmail.com>
2025-10-08 14:48:05 +0000

    a

--- program1/program1.c
+++ program1/program1.c
@@ -4,75 +4,103 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <signal.h>
-#include <string.h>
-#include <errno.h>
 
-int main(int argc, char *argv[]){
-        if (argc < 2) {
-                fprintf(stderr, "Usage: %s <test_program> [args...]\n", argv[0]);
-                exit(EXIT_FAILURE);
-        }
+static void report_signal(int signo)
+{
+	switch (signo) {
+	case 6:
+		printf("child process get SIGABRT signal\n");
+		break;
+	case 14:
+		printf("child process get SIGALRM signal\n");
+		break;
+	case 7:
+		printf("child process get SIGBUS signal\n");
+		break;
+	case 8:
+		printf("child process get SIGFPE signal\n");
+		break;
+	case 1:
+		printf("child process get SIGHUP signal\n");
+		break;
+	case 4:
+		printf("child process get SIGILL signal\n");
+		break;
+	case 2:
+		printf("child process get SIGINT signal\n");
+		break;
+	case 9:
+		printf("child process get SIGKILL signal\n");
+		break;
+	case 13:
+		printf("child process get SIGPIPE signal\n");
+		break;
+	case 3:
+		printf("child process get SIGQUIT signal\n");
+		break;
+	case 11:
+		printf("child process get SIGSEGV signal\n");
+		break;
+	case 5:
+		printf("child process get SIGTRAP signal\n");
+		break;
+	case 15:
+		printf("child process get SIGTERM signal\n");
+		break;
+	default:
+		break;
+	}
+}
 
-        pid_t pid = fork();
-        if (pid < 0) {
-                perror("fork");
-                exit(EXIT_FAILURE);
-        } else if (pid == 0) {
-                char **child_argv = &argv[1];
-                execvp(child_argv[0], child_argv);
+static void run_child(int argc, char *argv[])
+{
+	int idx;
+	char *child_argv[argc];
 
-                if (errno == ENOENT && strchr(child_argv[0], '/') == NULL) {
-                        size_t len = strlen(child_argv[0]);
-                        char *prefixed = malloc(len + 3);
-                        if (prefixed == NULL) {
-                                perror("malloc");
-                                _exit(EXIT_FAILURE);
-                        }
-                        prefixed[0] = '.';
-                        prefixed[1] = '/';
-                        memcpy(prefixed + 2, child_argv[0], len + 1);
-                        child_argv[0] = prefixed;
-                        execvp(child_argv[0], child_argv);
-                }
+	for (idx = 1; idx < argc; ++idx)
+		child_argv[idx - 1] = argv[idx];
+	child_argv[argc - 1] = NULL;
 
-                perror("execvp");
-                _exit(EXIT_FAILURE);
-        }
+	printf("I'm the Child Process, my pid = %d\nChild process start to execute test program:\n", getpid());
+	execve(child_argv[0], child_argv, NULL);
+	_exit(EXIT_SUCCESS);
+}
 
-        int status;
-        do {
-                pid_t w = waitpid(pid, &status, WUNTRACED | WCONTINUED);
-                if (w == -1) {
-                        perror("waitpid");
-                        exit(EXIT_FAILURE);
-                }
+static void handle_parent(pid_t child_pid)
+{
+	int status;
 
-                if (WIFEXITED(status)) {
-                        printf("Child process (%d) terminated normally with exit status %d\n",
-                               pid, WEXITSTATUS(status));
-                } else if (WIFSIGNALED(status)) {
-                        printf("Child process (%d) terminated by signal %d (%s)%s\n",
-                               pid,
-                               WTERMSIG(status),
-                               strsignal(WTERMSIG(status)),
-#ifdef WCOREDUMP
-                               WCOREDUMP(status) ? " (core dumped)" : "");
-#else
-                               "");
-#endif
-                } else if (WIFSTOPPED(status)) {
-                        printf("Child process (%d) stopped by signal %d (%s)\n",
-                               pid,
-                               WSTOPSIG(status),
-                               strsignal(WSTOPSIG(status)));
-                        if (kill(pid, SIGCONT) == -1) {
-                                perror("kill(SIGCONT)");
-                                exit(EXIT_FAILURE);
-                        }
-                } else if (WIFCONTINUED(status)) {
-                        printf("Child process (%d) continued\n", pid);
-                }
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	printf("I'm the Parent Process, my pid = %d\n", getpid());
+	waitpid(child_pid, &status, WUNTRACED);
+	printf("Parent process receives SIGCHLD signal\n");
 
-        return 0;
+	if (WIFEXITED(status)) {
+		printf("Normal termination with EXIT STATUS = %d\n", WEXITSTATUS(status));
+	} else if (WIFSTOPPED(status)) {
+		printf("child process get SIGSTOP signal\n");
+	} else {
+		report_signal(status & 127);
+	}
+
+	exit(0);
+}
+
+int main(int argc, char *argv[])
+{
+	pid_t pid;
+
+	printf("Process start to fork\n");
+	pid = fork();
+
+	if (pid < 0) {
+		perror("fork");
+		exit(1);
+	}
+
+	if (pid == 0)
+		run_child(argc, argv);
+	else
+		handle_parent(pid);
+
+	return 0;
 }
